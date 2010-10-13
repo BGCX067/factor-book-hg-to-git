fc9feb9a937ad03506fd6866852cf96e28eeeb65
Minh Van Nguyen <nguyenminh2@gmail.com>
2010-10-14 01:20:58 +1100

    bin/sdist.sh: typo fixes

--- bin/sdist.sh
+++ bin/sdist.sh
@@ -92,7 +92,7 @@ else
 fi
 
 # Test if there is a file called "book.pdf". That file results from
-# running the command "make" from GRAPH_ROOT.
+# running the command "make" from BOOK_ROOT.
 if [ -e "book.pdf" ] && [ -f "book.pdf" ]; then
     cd ..
     cp -rf "$BOOK_ROOT" "$NAME$VERSION"
@@ -100,7 +100,7 @@ if [ -e "book.pdf" ] && [ -f "book.pdf" ]; then
     tar -jcf "$NAME$VERSION".tar.bz2 "$NAME$VERSION"
     rm -rf "$NAME$VERSION"
     # You should now be left with the original source tree named
-    # "graph-theory-x.y". In addition, you now have two new files named
+    # "factorize-x.y". In addition, you now have two new files named
     # "<NAME><VERSION>.tar.bz2" and "<NAME><VERSION>.pdf".
 else
     echo "File book.pdf does not exist. Exiting..."
